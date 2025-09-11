39682895b054409c0353319cc9acbdef8f9bf374
amigazen <amigazen@noreply.github.com>
2025-09-11 11:55:28 +0100

    Added fully POSIX compliant true and false commands

--- src/true/true.c
+++ src/true/true.c
@@ -1,4 +1,36 @@
-main()
+/*
+ * true - unsui POSIX runtime for Amiga
+ * 
+ * Copyright (c) 2025 amigazen project. All rights reserved.
+ * 
+ * SPDX-License-Identifier: BSD-2-Clause
+ * See LICENSE.md for full license text.
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <exec/types.h>
+#include <dos/dos.h>
+
+#include <proto/dos.h>
+#include <proto/exec.h>
+
+static const char *verstag = "$VER: true 1.0 (11/09/25)\n";
+
+extern struct DosLibrary *DOSBase;
+
+int main(int argc, char **argv)
 {
-    exit(0);
+    /* Initialize DOSBase */
+    DOSBase = (struct DosLibrary *)OpenLibrary("dos.library", 0);
+    if (!DOSBase) {
+        return EXIT_FAILURE;
+    }
+    
+    /* Clean up */
+    CloseLibrary((struct Library *)DOSBase);
+    
+    /* Return success status as per POSIX specification */
+    return EXIT_SUCCESS;
 }
